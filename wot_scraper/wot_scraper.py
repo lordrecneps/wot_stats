53fcb3e3aa4178c7985e7ffc4f2e855aa8e8a887
Spencer Ong <spencer.pg@gmail.com>
2015-07-09 00:08:06 +0100

    added async io

--- wot_scraper/wot_scraper.py
+++ wot_scraper/wot_scraper.py
@@ -2,32 +2,37 @@ import urllib.request as web
 import json
 import pprint
 import sqlite3
+import asyncio
+import aiohttp
 from timeit import default_timer as timer
 
 tank_stat_url = 'http://api.worldoftanks.com/wot/tanks/stats/'
 stat_fields = ['tank_id', 'all.battles', 'all.damage_dealt', 'all.frags', 'all.spotted','all.wins']
 
 
-account_id = 1000185415
+start_id = 1000185415
 queries = {
     'application_id': 'ca49fa564ed39d6a5af35af7725beda2',
     'fields': ','.join(stat_fields),
-    'account_id': str(account_id)
+    'account_id': str(start_id)
 }
 
 start_time = timer()
 tank_stats = []
 players = []
-for id in range(account_id, account_id + 20):
-    queries['account_id'] = str(id)
-    stat_url = tank_stat_url + '?' + '&'.join([k + '=' + queries[k] for k in queries])
+sem = asyncio.Semaphore(10)
 
-    query_json = web.urlopen(stat_url)
-    query_data = json.loads(query_json.read().decode())
+@asyncio.coroutine
+def get_tank_data(url):
+    response = yield from aiohttp.request('GET', url)
+    return (yield from response.read_and_close(decode=True))
 
+def proc_tank_data(query_data, account_id):
+    #print(query_json)
+    #query_data = json.loads(query_json)
     if query_data['status'] == 'error':
         print('yo', query_data)
-        continue
+        return
 
     player_data = query_data['data']
     
@@ -37,6 +42,21 @@ for id in range(account_id, account_id + 20):
         players.append(tuple([int(p)]))
         for tank in player_data[p]:
             tank_stats.append( tuple([p, tank['tank_id']] + [tank['all'][k] for k in sorted(tank['all'].keys())]) )
+
+@asyncio.coroutine
+def iterate_tank_data(account_id):
+    #print(account_id)
+    queries['account_id'] = str(account_id)
+
+    stat_url = tank_stat_url + '?' + '&'.join([k + '=' + queries[k] for k in queries])
+    
+    with (yield from sem):
+        query_json = yield from get_tank_data(stat_url)
+    proc_tank_data(query_json, account_id)
+
+loop = asyncio.get_event_loop()
+f = asyncio.wait([iterate_tank_data(account_id) for account_id in range(start_id, start_id + 1000)])
+loop.run_until_complete(f)
 
 conn = sqlite3.connect('wot_stats.db')
 c = conn.cursor()
