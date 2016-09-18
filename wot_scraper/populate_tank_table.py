be9b9a6627e6b285c83f4b911e57de8932a21ff4
Spencer McLaughlin <spencer.pg@gmail.com>
2016-09-18 22:48:20 +0100

    Use encyclopedia/vehicles for tank update

--- wot_scraper/populate_tank_table.py
+++ wot_scraper/populate_tank_table.py
@@ -3,8 +3,8 @@ import json
 import psycopg2
 from os import environ
 
-tank_stat_url = 'http://api.worldoftanks.com/wot/encyclopedia/tanks/'
-stat_fields = ['name_i18n', 'nation_i18n', 'type', 'level']
+tank_stat_url = 'http://api.worldoftanks.com/wot/encyclopedia/vehicles/'
+stat_fields = ['name', 'nation', 'type', 'tier']
 
 queries = {
   'application_id': '74ce2b0de9bc463e29fce5910819d19a',
@@ -15,7 +15,7 @@ stat_url = tank_stat_url + '?' + '&'.join([k + '=' + queries[k] for k in queries
 
 def main():
   conn = psycopg2.connect("dbname='{}' user='{}' host='{}' port={} password='{}'".format(
-    environ['DPGWHORES_DBNAME'], environ['POSTGRES_USERNAME'], environ['POSTGRES_HOST'], 
+    environ['DPGWHORES_DBNAME'], environ['POSTGRES_USERNAME'], environ['POSTGRES_HOST'],
     environ['POSTGRES_PORT'], environ['POSTGRES_PW']
   ))
   c = conn.cursor()
@@ -40,12 +40,12 @@ def main():
     update_str.append(update_vals)
     update_str.append(''') as c(tank_id, name, nation, type, tier) where tanks.tank_id = c.tank_id''')
     c.execute(' '.join(update_str))
-    
+
     insert_str = ['''insert into tanks(tank_id, name, nation, type, tier) (select * from (values''']
     insert_str.append(update_vals)
     insert_str.append(''') as c(tank_id, name, nation, type, tier) where not exists (select 1 from tanks where tanks.tank_id = c.tank_id ))''')
     c.execute(' '.join(insert_str))
-          
+
     #c.executemany('''insert into tanks(tank_id, name, nation, type, tier) values(%s,%s,%s,%s,%s)
     #                 on conflict (tank_id) do update set name = excluded.name, nation = excluded.nation, type = excluded.type, tier = excluded.tier''', tanks)
     conn.commit()
