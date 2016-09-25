99550f88a3839f5bb5811413f0eb7c25460f67b8
Spencer McLaughlin <spencer.pg@gmail.com>
2016-09-25 23:00:09 +0100

    Fix IntegrityError and ProgrammingError bugs
    Fix bug where the same account_id is inserted
    twice in the same insert statement.
    
    Fix bug where zero accounts are added in a
    values() statement.

--- wot_scraper/populate_player_table.py
+++ wot_scraper/populate_player_table.py
@@ -6,8 +6,11 @@ from os import environ
 import sys
 from timeit import default_timer as timer
 import time
+import threading
 
 from common import app_ids, player_proxy_urls, sems, num_apps
+
+lock = threading.Lock()
 
 tank_stat_url = 'http://api.worldoftanks.com/wot/account/info/'
 stat_fields = ['nickname', 'statistics.random.battles', 'last_battle_time']
@@ -28,7 +31,7 @@ def get_player_nicks(ids, app_idx):
   return (yield from response.json())
 
 @asyncio.coroutine
-def proc_account_names(id_list, results, idx, c, conn):
+def proc_account_names(id_list, results, id_done, idx, c, conn):
   sem_idx = idx % 1 # num_apps
   try:
     with (yield from sems[sem_idx]):
@@ -51,8 +54,12 @@ def proc_account_names(id_list, results, idx, c, conn):
         'b': int(r_stats['random']['battles']),
         'l': int(res['last_battle_time'])
       }
-      if result['b'] >= 250:
-        results.append(result)
+      with lock:
+        if result['a'] in id_done:
+          continue
+        if result['b'] >= 250:
+          results.append(result)
+          id_done.add(result['a'])
 
   except Exception as e:
     print(e, 'Exception')
@@ -99,24 +106,26 @@ def update_loop(c, conn, start_point):
         break
 
     acc_names = []
+    id_done = set()
     loop = asyncio.get_event_loop()
     f = asyncio.wait([
-      proc_account_names(id_list, acc_names, idx, c, conn)
+      proc_account_names(id_list, acc_names, id_done, idx, c, conn)
       for idx, id_list in enumerate(id_lists)
     ])
     loop.run_until_complete(f)
     print('                             ', end='\r')
     print('loop: ', b, end='\r')
 
-    insert_str = ['''
-      insert into players2(account_id, nickname, battles)
-      (select * from (values''']
-    insert_vals = ','.join(c.mogrify("(%(a)s,%(n)s,%(b)s)", x).decode('utf-8') for x in acc_names)
-    insert_str.append(insert_vals)
-    insert_str.append(''') as c(a, n, b)
-      where not exists (select 1 from players2 where account_id = c.a))''')
-    c.execute(' '.join(insert_str))
-    conn.commit()
+    if len(acc_names) > 0:
+      insert_str = ['''
+        insert into players2(account_id, nickname, battles)
+        (select * from (values''']
+      insert_vals = ','.join(c.mogrify("(%(a)s,%(n)s,%(b)s)", x).decode('utf-8') for x in acc_names)
+      insert_str.append(insert_vals)
+      insert_str.append(''') as c(a, n, b)
+        where not exists (select 1 from players2 where account_id = c.a))''')
+      c.execute(' '.join(insert_str))
+      conn.commit()
     start_point[0] = end
 
   print(timer() - start_time)
