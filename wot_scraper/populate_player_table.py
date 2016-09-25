5d5b328390a11155e7f49fbec9f7905a72593387
Spencer McLaughlin <spencer.pg@gmail.com>
2016-09-25 01:14:52 +0100

    Rework script to find/add new players

--- wot_scraper/populate_player_table.py
+++ wot_scraper/populate_player_table.py
@@ -10,7 +10,7 @@ import time
 from common import app_ids, player_proxy_urls, sems, num_apps
 
 tank_stat_url = 'http://api.worldoftanks.com/wot/account/info/'
-stat_fields = ['nickname', 'statistics.random.battles']
+stat_fields = ['nickname', 'statistics.random.battles', 'last_battle_time']
 queries = {
   'application_id': 'ca49fa564ed39d6a5af35af7725beda2',
   'fields': ','.join(stat_fields),
@@ -23,86 +23,55 @@ def get_player_nicks(ids, app_idx):
   queries['account_id'] = ids
   queries['application_id'] = app_ids[app_idx]
   stat_url = tank_stat_url + '?' + '&'.join([k + '=' + queries[k] for k in queries])
-  payload = {'a': stat_url}
-  response = yield from asyncio.wait_for(aiohttp.post(player_proxy_urls[app_idx], data=payload), 10)
-  #response = yield from aiohttp.get(tank_stat_url, params=queries)
+
+  response = yield from asyncio.wait_for(aiohttp.get(tank_stat_url, params=queries), 10)
   return (yield from response.json())
 
 @asyncio.coroutine
 def proc_account_names(id_list, results, idx, c, conn):
-  sem_idx = idx % num_apps
+  sem_idx = idx % 1 # num_apps
   try:
     with (yield from sems[sem_idx]):
       response = yield from get_player_nicks(id_list, sem_idx)
-    
+
     if response['status'] == 'error':
       print('Error response received:', response)
-      for account_id in [int(x) for x in id_list.split(',')]:
-        c.execute('''
-          insert into pfailed(account_id) (select * from (values (%(a)s) ) as bla
-          where not exists (select 1 from pfailed where account_id = %(a)s))''',
-          {'a': account_id}
-        )
-      conn.commit()
       return
-    
+
     res_data = response['data']
     for id in res_data:
       if not res_data[id]:
         continue
       res = res_data[id]
       r_stats = res['statistics']
-      
+
       result = {
         'a': int(id),
         'n': res['nickname'],
-        'b': int(r_stats['random']['battles'])
+        'b': int(r_stats['random']['battles']),
+        'l': int(res['last_battle_time'])
       }
       if result['b'] >= 250:
         results.append(result)
-      
+
   except Exception as e:
     print(e, 'Exception')
-    for account_id in [int(x) for x in id_list.split(',')]:
-      c.execute('''
-        insert into pfailed(account_id) (select * from (values (%(a)s) ) as bla
-        where not exists (select 1 from pfailed where account_id = %(a)s))''',
-        {'a': account_id}
-      )
-    conn.commit()
   except:
     print(sys.exc_info(), 'Exception(sys):')
-    for account_id in [int(x) for x in id_list.split(',')]:
-      c.execute('''
-        insert into pfailed(account_id) (select * from (values (%(a)s) ) as bla
-        where not exists (select 1 from pfailed where account_id = %(a)s))''',
-        {'a': account_id}
-      )
-    conn.commit()
 
-def update_loop(c, conn, wakeup=False, proc_fails=False):
-  start_id = 1000000000
+def update_loop(c, conn, start_point):
+  start_id = start_point[0]
   batch_size = 1000
-  max_account = 1018700000
+  c.execute('select max(account_id) from players2')
+  max_account = c.fetchone()
 
-  player_ids = []
+  if not max_account:
+    max_account = 1023000000
+  else:
+    max_account = max(max_account[0], 1023000000)
+
+  max_account = max_account + 250000
   player_id_num = max_account - start_id
-
-  if proc_fails:
-    start_id = 0
-    
-    c.execute('select account_id from pfailed')
-    player_ids = c.fetchall()
-    
-    if not player_ids:
-      return 1
-    
-    c.execute('delete from pfailed')
-    conn.commit()
-    
-    player_id_num = len(player_ids)
-    max_account = player_id_num
-
 
   num_requests = int( ceil(player_id_num / 100) )
   print('Number of accounts to process:', player_id_num)
@@ -117,21 +86,18 @@ def update_loop(c, conn, wakeup=False, proc_fails=False):
     end = start_id + (b + 1) * 100 * batch_size
     print('Batch:', b, end='\r')
     id_lists = []
-    
+
     for b_start in range(start, end, 100):
       b_end = b_start + 100
-      
+
       if b_end > max_account:
         b_end = max_account
-      
-      if proc_fails:
-        id_lists.append(','.join( [str(ri[0]) for ri in player_ids[b_start:b_end]] ))
-      else:
-        id_lists.append(','.join( [str(ri) for ri in range(b_start, b_end)] ))
-      
+
+      id_lists.append(','.join( [str(ri) for ri in range(b_start, b_end)] ))
+
       if b_end == max_account:
         break
-    
+
     acc_names = []
     loop = asyncio.get_event_loop()
     f = asyncio.wait([
@@ -141,12 +107,7 @@ def update_loop(c, conn, wakeup=False, proc_fails=False):
     loop.run_until_complete(f)
     print('                             ', end='\r')
     print('loop: ', b, end='\r')
-    
-    if wakeup:
-      c.execute('delete from pfailed')
-      conn.commit()
-      return 0
-    
+
     insert_str = ['''
       insert into players2(account_id, nickname, battles)
       (select * from (values''']
@@ -156,53 +117,33 @@ def update_loop(c, conn, wakeup=False, proc_fails=False):
       where not exists (select 1 from players2 where account_id = c.a))''')
     c.execute(' '.join(insert_str))
     conn.commit()
+    start_point[0] = end
 
   print(timer() - start_time)
 
 def update_players():
   conn = psycopg2.connect("dbname='{}' user='{}' host='{}' port={} password='{}'".format(
-    environ['DPGWHORES_DBNAME'], environ['POSTGRES_USERNAME'], environ['POSTGRES_HOST'], 
+    environ['DPGWHORES_DBNAME'], environ['POSTGRES_USERNAME'], environ['POSTGRES_HOST'],
     environ['POSTGRES_PORT'], environ['POSTGRES_PW']
   ))
   c = conn.cursor()
-
-  print("Waking proxies up")
-  update_loop(c, conn, wakeup=True)
-  time.sleep(10)
-  print("Second waking")
-  update_loop(c, conn, wakeup=True)
-  time.sleep(5)
-  
   done = False
-  
+  start_point = [1000000000]
+
   for _ in range(10):
     try:
       print("Starting player update")
       conn.commit()
-      update_loop(c, conn)
+      update_loop(c, conn, start_point)
       print("Finished player update")
       done = True
       break
     except:
       print(sys.exc_info(), 'Exception(sys)')
-  done = True
-  if done:
-    done = False
-    print("Processing player fails")
-    for _ in range(10):
-      try:
-        for _ in range(10):
-          conn.commit()
-          if update_loop(c, conn, proc_fails=True) == 1:
-            break
-        done = True
-        break
-      except:
-        print(sys.exc_info(), 'Exception(sys)')
-  
+
   conn.commit()
   conn.close()
-  
+
   if done:
     return 1
   else:
