d0b66551f70afd0d793851eaf2f66339f69c71d9
Spencer McLaughlin <spencer.pg@gmail.com>
2017-01-01 01:10:07 +0000

    Only use accounts with >50 battles for percentiles
    Use list extend

--- wot_scraper/compute_percentiles.py
+++ wot_scraper/compute_percentiles.py
@@ -8,13 +8,12 @@ conn = psycopg2.connect("dbname='{}' user='{}' host='{}' port={} password='{}'".
 c = conn.cursor()
 
 vals = [0.01*x for x in range(100)]
-vals.extend([0.001*x for x in range(1, 10)])
 
 c.execute('select distinct tank_id, tank_name from top50')
 tank_ids = c.fetchall()
 
 for tid, tname in tank_ids:
-	c.execute('select percentile_cont(%s) within group(order by dpg desc nulls last) from dpg2 where tank_id = %s and battles >= 25', (vals, tid))
+	c.execute('select percentile_cont(%s) within group(order by dpg desc nulls last) from dpg2 where tank_id = %s and battles > 50', (vals, tid))
 	
 	for idx, dpg in enumerate(c.fetchall()[0][0]):
 		c.execute('insert into percentiles(tank_id, dpg, pct) values(%s, %s, %s)', (tid, dpg, 1.0-vals[idx]))
