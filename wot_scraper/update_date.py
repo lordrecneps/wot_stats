ec9a9db9a8a1d0ff34152af298afaa425b5dc137
Spencer McLaughlin <spencer.pg@gmail.com>
2017-01-01 01:02:59 +0000

    Modify date update to handle eu timestamps

--- wot_scraper/update_date.py
+++ wot_scraper/update_date.py
@@ -3,16 +3,26 @@ from datetime import timedelta
 from datetime import datetime
 from datetime import timezone
 
-def main():
+def main(server='na'):
 	curr_time = datetime.now(timezone.utc)
-	est_diff = timedelta(hours=-5)
+	if server == 'na':
+		est_diff = timedelta(hours=-5)
+	else:
+		est_diff = timedelta(hours=0)
 	update_time = curr_time + est_diff
 
-	repl_html = '<span id="last_update">Last update: ' + update_time.strftime('%d %b %H:%M EST') + '</span>'
+	if server == 'na':
+		repl_html = '<span id="last_update">Last update NA: ' + update_time.strftime('%d %b %H:%M EST') + ' | EU'
+	else:
+		repl_html = '| EU: ' + update_time.strftime('%d %b %H:%M GMT') + '</span>'
 
 	with open('templates/master.html', 'r') as f:
 		master_html = f.read()
-		new_html = re.sub(r'<span id="last_update">.*?</span>', repl_html, master_html)
+
+		if server == 'na':
+			new_html = re.sub(r'<span id="last_update">.*?\| EU', repl_html, master_html)
+		else:
+			new_html = re.sub(r'\| EU.*?</span>', repl_html, master_html)
 	
 	with open('templates/master.html', 'w') as f:
 		f.write(new_html)
