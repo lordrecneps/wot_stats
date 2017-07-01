909c03719267a5c037bcb8e689dc37c0fb109d75
Spencer McLaughlin <spencer.pg@gmail.com>
2017-07-02 00:37:41 +0100

    Use both new and deprecated API for tankopedia
    In their infinite wisdom, WG has only added new tanks to their
    deprecated API, so we need to use both to get a complete list of all
    tanks.

--- wot_scraper/populate_tank_table.py
+++ wot_scraper/populate_tank_table.py
@@ -1,55 +1,71 @@
 import urllib.request as web
 import json
-import psycopg2
-from os import environ
+from common import pg_connect
 
-tank_stat_urls = {'na': 'http://api.worldoftanks.com/wot/encyclopedia/vehicles/',
-                  'eu': 'http://api.worldoftanks.eu/wot/encyclopedia/vehicles/'}
+# Unfortunately we need to use the deprecated encyclopedia/tanks API since new tanks like the Chrysler haven't been
+# added to the 'new' API
+tank_stat_urls = {'na': ['http://api.worldoftanks.com/wot/encyclopedia/tanks/',
+                         'http://api.worldoftanks.com/wot/encyclopedia/vehicles/'],
+                  'eu': ['http://api.worldoftanks.eu/wot/encyclopedia/tanks/',
+                         'http://api.worldoftanks.eu/wot/encyclopedia/vehicles/']}
 stat_fields = ['name', 'nation', 'type', 'tier']
+deprecated_stat_fields = ['name_i18n', 'nation_i18n', 'type', 'level']
 
 queries = {
   'application_id': '74ce2b0de9bc463e29fce5910819d19a',
   'fields': ','.join(stat_fields)
 }
 
+deprecated_queries = {
+  'application_id': '74ce2b0de9bc463e29fce5910819d19a',
+  'fields': ','.join(deprecated_stat_fields)
+}
+
 def main(server='na'):
-  tank_stat_url = tank_stat_urls[server]
+    tank_stat_url = tank_stat_urls[server]
 
-  conn = psycopg2.connect("dbname='{}' user='{}' host='{}' port={} password='{}'".format(
-    environ['DPGWHORES_DBNAME'], environ['POSTGRES_USERNAME'], environ['POSTGRES_HOST'],
-    environ['POSTGRES_PORT'], environ['POSTGRES_PW']
-  ))
-  c = conn.cursor()
+    conn, c = pg_connect()
 
-  stat_url = tank_stat_url + '?' + '&'.join([k + '=' + queries[k] for k in queries])
-  result = json.loads(web.urlopen(stat_url).readall().decode('utf-8'))
+    stat_url = tank_stat_url[0] + '?' + '&'.join([k + '=' + deprecated_queries[k] for k in deprecated_queries])
+    deprecated_result = json.loads(web.urlopen(stat_url).readall().decode('utf-8'))
 
-  tanks = []
+    stat_url = tank_stat_url[1] + '?' + '&'.join([k + '=' + queries[k] for k in queries])
+    result = json.loads(web.urlopen(stat_url).readall().decode('utf-8'))
 
-  if result['status'] == 'error':
-    print('Error: ', result)
-  else:
-    tank_data = result['data']
-    for id in tank_data:
-      if not tank_data[id]:
-        continue
-      result = [int(id)]
-      result.extend([ tank_data[id][k] for k in stat_fields ])
-      tanks.append( tuple(result) );
+    tanks = []
 
-    update_vals = ','.join(c.mogrify("(%s,%s,%s,%s,%s)", x).decode('utf-8') for x in tanks)
-    update_str = ['''update tanks set name = c.name, nation = c.nation, type = c.type, tier = c.tier from (values''']
-    update_str.append(update_vals)
-    update_str.append(''') as c(tank_id, name, nation, type, tier) where tanks.tank_id = c.tank_id''')
-    c.execute(' '.join(update_str))
+    if result['status'] == 'error':
+        print('Error: ', result)
+    else:
+        tank_data = result['data']
 
-    insert_str = ['''insert into tanks(tank_id, name, nation, type, tier) (select * from (values''']
-    insert_str.append(update_vals)
-    insert_str.append(''') as c(tank_id, name, nation, type, tier) where not exists (select 1 from tanks where tanks.tank_id = c.tank_id ))''')
-    c.execute(' '.join(insert_str))
+        for tank_id in tank_data:
+            if not tank_data[tank_id]:
+                continue
+            result = [int(tank_id)]
+            result.extend([ tank_data[tank_id][k] for k in stat_fields ])
+            tanks.append(tuple(result))
 
-    #c.executemany('''insert into tanks(tank_id, name, nation, type, tier) values(%s,%s,%s,%s,%s)
-    #                 on conflict (tank_id) do update set name = excluded.name, nation = excluded.nation, type = excluded.type, tier = excluded.tier''', tanks)
-    conn.commit()
+        if deprecated_result['status'] == 'ok':
+            deprecated_data = deprecated_result['data']
+            for tank_id in deprecated_data:
+                if tank_id in tank_data or not deprecated_data[tank_id]:
+                    continue
+                result = [int(tank_id)]
+                result.extend([ deprecated_data[tank_id][k] for k in deprecated_stat_fields ])
+                tanks.append(tuple(result))
 
-  conn.close()
+
+        update_vals = ','.join(c.mogrify("(%s,%s,%s,%s,%s)", x).decode('utf-8') for x in tanks)
+        update_str = ['''update tanks set name = c.name, nation = c.nation, type = c.type, tier = c.tier from (values''']
+        update_str.append(update_vals)
+        update_str.append(''') as c(tank_id, name, nation, type, tier) where tanks.tank_id = c.tank_id''')
+        c.execute(' '.join(update_str))
+
+        insert_str = ['''insert into tanks(tank_id, name, nation, type, tier) (select * from (values''']
+        insert_str.append(update_vals)
+        insert_str.append(''') as c(tank_id, name, nation, type, tier) where not exists (select 1 from tanks where tanks.tank_id = c.tank_id ))''')
+        c.execute(' '.join(insert_str))
+        conn.commit()
+
+    conn.close()
